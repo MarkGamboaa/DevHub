b7e86870b787568f1850b6f8763d6c696b13f64f
MarkGamboa <202310110@fit.edu.ph>
2025-08-03 22:07:09 +0800

    refactor: Update asset paths and replace SVG icon with image for location

--- src/App.jsx
+++ src/App.jsx
@@ -125,7 +125,7 @@ export default function App() {
       <div className="w-full flex flex-col items-center">
         {/* Logo above DevHub title */}
         <img
-          src={theme === 'dark' ? '/src/assets/logo_dark.png' : '/src/assets/logo_light.png'}
+          src={theme === 'dark' ? '/logo_dark.png' : '/logo_light.png'}
           alt="DevHub Logo"
           width={64}
           height={64}
@@ -221,10 +221,7 @@ export default function App() {
                   </div>
                   {user.location && (
                     <div className={`flex items-center gap-1 px-3 py-1 rounded-lg border ${theme === 'dark' ? 'bg-[#161b22] border-[hsl(222,14%,25%)] text-[hsl(210,10%,88%)]' : 'bg-[hsl(210,16%,96%)] border-[hsl(210,16%,88%)] text-[hsl(210,10%,23%)]'}`}>
-                      {/* Custom location icon */}
-                      <svg viewBox="0 0 32 32" width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg" className="inline text-inherit" style={{color: 'currentColor'}}>
-                        <path d="M16.114-0.011c-6.559 0-12.114 5.587-12.114 12.204 0 6.93 6.439 14.017 10.77 18.998 0.017 0.020 0.717 0.797 1.579 0.797h0.076c0.863 0 1.558-0.777 1.575-0.797 4.064-4.672 10-12.377 10-18.998 0-6.618-4.333-12.204-11.886-12.204zM16.515 29.849c-0.035 0.035-0.086 0.074-0.131 0.107-0.046-0.032-0.096-0.072-0.133-0.107l-0.523-0.602c-4.106-4.71-9.729-11.161-9.729-17.055 0-5.532 4.632-10.205 10.114-10.205 6.829 0 9.886 5.125 9.886 10.205 0 4.474-3.192 10.416-9.485 17.657zM16.035 6.044c-3.313 0-6 2.686-6 6s2.687 6 6 6 6-2.687 6-6-2.686-6-6-6zM16.035 16.044c-2.206 0-4.046-1.838-4.046-4.044s1.794-4 4-4c2.207 0 4 1.794 4 4 0.001 2.206-1.747 4.044-3.954 4.044z" fill="currentColor" />
-                      </svg>
+                      <img src="/location.svg" alt="Location" width={16} height={16} className="inline" style={{color: 'currentColor'}} />
                       <span className="font-semibold">{user.location}</span>
                     </div>
                   )}
